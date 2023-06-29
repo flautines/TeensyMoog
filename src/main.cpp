24462ca9d6d98593e6caeb895fb005cced23bbfe
flautines <flautines@live.com>
2023-06-29 07:50:11 +0200

    First commit of tddstrict branch.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,11 @@
 #include <Arduino.h>
-#include "moog.h"
 
 MoogSynth minimoog;
 
 void setup() {
-	minimoog.begin();
-	minimoog.range[0] = RANGE_16;
-	minimoog.range[1] = RANGE_8;
-	minimoog.range[2] = RANGE_4;
+
 }
 
 void loop() {
-	minimoog.update();
+
 }
---
+++ test/test_moog/test_moog.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+#include <unity.h>
+
+void setUp()
+{
+
+}
+
+void tearDown()
+{
+
+}
+
+void setup()
+{
+	UNITY_BEGIN();
+	
+	UNITY_END();
+}
+
+void loop()
+{
+
+}
